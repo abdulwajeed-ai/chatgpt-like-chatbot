6ce81fcfb67bd021274f469530855ff1779f0a3f
abdulwajeed-ai <abdulwajeed.ai.dev@gmail.com>
2025-08-22 10:20:18 +0530

    updating the socket io code

--- server/src/sockets/socketServer.js
+++ server/src/sockets/socketServer.js
@@ -1,13 +1,32 @@
 const { Server } = require("socket.io");
 const { init } = require("../models/authModel");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const userModel = require("../models/chatModel");
 
 const initSocketServer = (httpServer) => {
   const io = new Server(httpServer, {});
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+    if (!cookies.token) {
+      next(new Error("Authentication error: no token provided"));
+    }
 
-  io.on("connection", (socket) => {
-    console.log("New socket connection:", socket);
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+      socket.user = decoded;
+      next();
+    } catch (err) {
+      next(new Error("Authentication error: Invalid Token"));
+    }
   });
-
+  io.on("connection", (socket) => {
+    console.log("User connected", socket.user);
+    console.log("New socket connection:", socket.id);
+  });
 };
 
 module.exports = initSocketServer;
